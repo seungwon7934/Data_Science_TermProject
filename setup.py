e19cd638658a42c31badb1571b39306b6c032daa
SeungWon Lee <96871857+seungwon7934@users.noreply.github.com>
2023-06-09 16:03:41 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='library',
+    name='help-processing',
     version='0.0.1',
     description='Functions about Data Science',
     url='https://github.com/seungwon7934/Data_Science_TermProject',
     author='SeungwonLee',
     author_email='sw7934@gmail.com',
     install_requires=['scikit-learn', 'numpy', 'pandas', 'seaborn', 'matplotlib', 'yellowbrick'],
-    packages=['library']
+    packages=['help-processing']
 )
